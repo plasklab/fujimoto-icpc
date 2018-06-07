03e7f44454778cf79e136d4edff4e56765abf365
thmyon <zz-0730-zz1937@docomo.ne.jp>
2018-06-07 16:08:08 +0900

    push my solution

--- ALDS1/4_A/src/Main.java
+++ ALDS1/4_A/src/Main.java
@@ -1,18 +1,27 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //ALDS1_4_A
 public class Main {
 	void run() {
+		int count = 0;
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+		int[] S = new int[n];
 		for (int i = 0; i < n; i++) {
-			
+			S[i] = sc.nextInt();
 		}
 		int q = sc.nextInt();
 		for (int i = 0; i < q; i++) {
-			
+			int cmp = sc.nextInt();
+			for (int k = 0; k < S.length; k++) {
+				if (cmp == S[k]) {
+					count++;
+					break;
+				}
+			}
 		}
+		System.out.println(count);
 	}
 
 	public static void main(String[] args) {
